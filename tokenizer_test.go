5d9759789786624ab7e1fdb2424af59adf78073a
Mateus Gomes <mateusg.18@gmail.com>
2014-08-31 03:21:12 -0300

    Use map comparison helper on tokenizer test

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,17 +1,27 @@
 package shield
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestTokenize(t *testing.T) {
-
 	tokenizer := NewEnglishTokenizer()
 	text := "lorem    ipsum able hello erik    can do hi there  \t  spaaace! lorem"
 	m := tokenizer.Tokenize(text)
-	x := fmt.Sprintf("%v", m)
-	if x != `map[lorem:2 ipsum:1 hello:1 erik:1 spaaace:1]` {
-		t.Fatal(x)
-	}
+
+  expectedMap := map[string]int64{ "lorem": 2, "ipsum": 1, "hello": 1, "erik": 1, "spaaace": 1 }
+  if errorMessage := compareMaps(m, expectedMap); errorMessage != "" {
+    t.Fatal(errorMessage)
+  }
+}
+
+func TestTokenizeWithPartialMatch(t *testing.T) {
+  tokenizer := NewEnglishTokenizer()
+  text := "lorem    ipsum able hello erik   cannon can do hi there  \t  spaaace! lorem"
+  m := tokenizer.Tokenize(text)
+
+  expectedMap := map[string]int64{ "lorem": 2, "ipsum": 1, "hello": 1, "erik": 1, "spaaace": 1, "cannon": 1 }
+  if errorMessage := compareMaps(m, expectedMap); errorMessage != "" {
+    t.Fatal(errorMessage)
+  }
 }
